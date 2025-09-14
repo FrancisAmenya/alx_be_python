de9455223209ede5793ff08a1f2668bc967c9fdc
FrancisAmenya <amenyafrancis@gmail.com>
2025-09-14 07:20:24 -0700

    modified variable to easier names

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,10 +1,10 @@
 # Assigning values to the variables
-P = 1000
-R = 0.05
-T = 3
+principal = 1000
+rate = 0.05
+time = 3
 
 # Performing simple interest calculations on python3
-I = P * R * T
+interest = principal * interest * time
 
 # Printing the results in the required format
-print(f"The simple interest is {I}")
+print(f"The simple interest is {interest}")
