af5f6bff00e4573d2c842f57c923cbc173f21f40
FrancisAmenya <amenyafrancis@gmail.com>
2025-09-14 07:23:47 -0700

    corrected the formula for simple interest calculation

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -4,7 +4,7 @@ rate = 0.05
 time = 3
 
 # Performing simple interest calculations on python3
-interest = principal * interest * time
+interest = principal * rate * time
 
 # Printing the results in the required format
 print(f"The simple interest is {interest}")
