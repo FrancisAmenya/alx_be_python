552cbf73dc1494d3588ee26f8e742931f39c75c9
FrancisAmenya <amenyafrancis@gmail.com>
2025-09-14 07:10:57 -0700

    added a simple interest calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# Assigning values to the variables
+P = 1000
+R = 0.05
+T = 3
+
+# Performing simple interest calculations on python3
+I = P * R * T
+
+# Printing the results in the required format
+print(f"The simple interest is {I}")
