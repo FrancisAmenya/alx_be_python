9eeceecdcd0d7f2461ebaf96d9a5636c9a4987e7
FrancisAmenya <amenyafrancis@gmail.com>
2025-09-18 09:19:41 -0700

    used a different print formatted statement as per requirements

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -6,10 +6,6 @@ time_bound = input("Is it time-bound? (yes/no): ")
 reminder_a = "'" + task + "'"
 reminder_no = ". Consider completing it when you have free time."
 reminder_yes = " that requires immediate attention today!"
-reminder_b = ""
-reminder_c = ""
-reminder_d = "Note: "
-reminder_e = "Reminder: "
 
 priority_high = " is a high priority task"
 priority_medium = " is a medium priority task"
@@ -17,15 +13,13 @@ priority_low = " is a low priority task"
 
 match priority:
 	case "high":
-		reminder_b = reminder_a + priority_high
+		reminder_a = reminder_a + priority_high
 	case "low":
-		reminder_b = reminder_a + priority_low
+		reminder_a = reminder_a + priority_low
 	case "medium":
-		reminder_b = reminder_a + priority_medium
+		reminder_a = reminder_a + priority_medium
 
 if time_bound == "yes":
-	reminder_c = reminder_e + reminder_b + reminder_yes
+	print(f"'Reminder: ' + {reminder_a} + {reminder_yes}")
 elif time_bound == "no":
-	reminder_c = reminder_d + reminder_b + reminder_no
-
-print(f"{reminder_c}")
+        print(f"'Note: ' + {reminder_a} + {reminder_no}")
