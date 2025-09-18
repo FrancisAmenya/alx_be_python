10c5fed3c53d2400dcf122219a223d819050d492
FrancisAmenya <amenyafrancis@gmail.com>
2025-09-18 09:04:13 -0700

    created a daily reminder script

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,31 @@
+# Prompt the user for input
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ")
+timeliness = input("Is it time-bound? (yes/no): ")
+
+reminder_a = "'" + task + "'"
+reminder_no = ". Consider completing it when you have free time."
+reminder_yes = " that requires immediate attention today!"
+reminder_b = ""
+reminder_c = ""
+reminder_d = "Note: "
+
+priority_high = " is a high priority task"
+priority_medium = " is a medium priority task"
+priority_low = " is a low priority task"
+
+match priority:
+	case "high":
+		reminder_b = reminder_a + priority_high
+	case "low":
+		reminder_b = reminder_a + priority_low
+	case "medium":
+		reminder_b = reminder_a + priority_medium
+
+match timeliness:
+	case "yes":
+		reminder_c = reminder_b + reminder_yes
+	case "no":
+		reminder_c = reminder_d + reminder_b + reminder_no
+
+print(reminder_c)
