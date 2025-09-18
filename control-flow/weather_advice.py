ea79a461bebc2bcdca00ec3d37e2fd751edbfca1
FrancisAmenya <amenyafrancis@gmail.com>
2025-09-18 05:36:15 -0700

    fixing syntax and grammar errors

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,15 +1,15 @@
 # Prompt the user to enter their current weather
-current_weather = input("What's the weather like today? (sunny/rainy/cold): ")
+weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
 # Providing a recommendation
 recommendation = ""
 
-if current_weather == "sunny":
+if weather == "sunny":
 	recommendation = "Wear a t-shirt and sunglasses."
-elif current_weather == "rainy":
-	recommendation = "Don't forget your umbrellaand raincoat"
-elif current_weather == "cold":
-	recommendation = "Make sure to wear a warm coat and scarf"
+elif weather == "rainy":
+	recommendation = "Don't forget your umbrella and raincoat."
+elif weather == "cold":
+	recommendation = "Make sure to wear a warm coat and scarf."
 else:
 	recommendation = "Sorry, I don't hae recommendations for this weather."
 
