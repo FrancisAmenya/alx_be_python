6b6539e0ea01d0e9eec22bcbaf157f7c80efeb58
FrancisAmenya <amenyafrancis@gmail.com>
2025-09-18 08:21:24 -0700

    fixed the while loop to include increments for i and j

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,9 +1,11 @@
 # Prompt the user for the size of the square they want
 size = int(input("Enter the size of the pattern: "))
 
-i = 1
-while i < size + 1:
-	print("*" * size)
+i = 0
+while i < size:
+	j = 0;
+	while j < size:
+		print("*", end="")
+		j += 1
+	print()
 	i += 1
-	if i == size:
-		break
