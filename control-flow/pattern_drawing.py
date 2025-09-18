808081a7020a67636a09915111c1f4fea5b2ce1a
FrancisAmenya <amenyafrancis@gmail.com>
2025-09-18 07:52:54 -0700

    removed an extra whitespace when asking user for input

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,5 +1,5 @@
 # Prompt the user for the size of the square they want
-size = int(input("Enter size of the pattern: "))
+size = int(input("Enter size of the pattern:"))
 
 for i in range(1, size+1):
 	if i > size:
